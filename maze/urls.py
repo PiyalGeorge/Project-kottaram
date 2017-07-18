fae630cd8352b611f6b9146ac8d49421ba0fac3a
piyal <piyal.sayone@gmail.com>
2017-07-19 01:50:43 +0530

    Twitter data into models

--- maze/urls.py
+++ maze/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+
 from accounts.views import homeview
+
 
 urlpatterns = [
 
@@ -8,4 +12,5 @@ urlpatterns = [
     url(r'^account/', include('accounts.urls', namespace="account")),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', admin.site.urls),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
